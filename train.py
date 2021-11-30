0b8b261e61c13b9672d576a27c77b666909ad3f1
Donglin Liu <liudonglin301@gmail.com>
2021-11-29 20:06:42 -0500

    m

--- train.py
+++ train.py
@@ -4,5 +4,6 @@ from douzero.dmc import parser, train
 
 if __name__ == '__main__':
     flags = parser.parse_args()
+    print(flags.num_actors)
     os.environ["CUDA_VISIBLE_DEVICES"] = flags.gpu_devices
     train(flags)
