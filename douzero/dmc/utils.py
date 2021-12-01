a61f4150eda9069e3f40af6591baa7826e97376f
Donglin Liu <liudonglin301@gmail.com>
2021-11-30 19:10:05 -0500

    m

--- douzero/dmc/utils.py
+++ douzero/dmc/utils.py
@@ -222,14 +222,7 @@ def act1(i, device, free_queue, full_queue, model, buffers, flags):
             obs_z_buf[position].append(env_output['obs_z'])
             # with tf.stop_gradient():    #todo
             agent_output = model.forward(position, obs['z_batch'], obs['x_batch'], flags=flags)
-            print(agent_output['action'])
-            print(agent_output['action'])
-            print(agent_output['action'])
-            print(agent_output['action'])
-            print(agent_output['action'])
-            print(agent_output['action'])
-            print(agent_output['action'])
-            print(agent_output['action'])
+            
             _action_idx = int(agent_output['action'].numpy())
             action = obs['legal_actions'][_action_idx]
             obs_action_buf[position].append(_cards2tensor(action))
