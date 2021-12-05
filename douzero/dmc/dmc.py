479d2f6f57560c4f7dc5fc49b2156329377edc87
Donglin Liu <liudonglin301@gmail.com>
2021-12-05 18:47:45 -0500

    m

--- douzero/dmc/dmc.py
+++ douzero/dmc/dmc.py
@@ -163,7 +163,7 @@ def train(flags):
             _, _, free_queue[device], full_queue[device], model, buffers[device], flags = act1(0, device, free_queue[device], full_queue[device], model, buffers[device], flags)
 
             for position in ['landlord', 'landlord_up', 'landlord_down']:
-                if(len(full_queue[device][position]) < flags.batch_size):
+                if(full_queue[device][position].qsize < flags.batch_size):
                     continue
                 indices = [full_queue[device][position].get() for _ in range(flags.batch_size)]
                 batch = {
