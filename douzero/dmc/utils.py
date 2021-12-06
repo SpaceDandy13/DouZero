32f78c783ecfde635136936ed7c88ac041b866ff
Donglin Liu <liudonglin301@gmail.com>
2021-12-05 21:50:01 -0500

    m

--- douzero/dmc/utils.py
+++ douzero/dmc/utils.py
@@ -241,6 +241,7 @@ def act1(i, device, free_queue, full_queue, model, buffers, flags):
                     break
 
             for p in positions:
+                print(p, size[p])
                 while size[p] > T: 
                     index = free_queue[p].get()
                     if index is None:
@@ -261,7 +262,7 @@ def act1(i, device, free_queue, full_queue, model, buffers, flags):
                     obs_z_buf[p] = obs_z_buf[p][T:]
                     size[p] -= T
 
-            print(full_queue['landlord'].qsize(), full_queue['landlord_up'].qsize(), full_queue['landlord_down'].qsize())
+            # print(full_queue['landlord'].qsize(), full_queue['landlord_up'].qsize(), full_queue['landlord_down'].qsize())
             if full_queue['landlord'].qsize() < flags.batch_size and full_queue['landlord_up'].qsize() < flags.batch_size and full_queue['landlord_down'].qsize() < flags.batch_size:
                 break
 
